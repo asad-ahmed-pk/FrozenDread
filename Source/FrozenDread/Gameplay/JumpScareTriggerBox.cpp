d8dbb036a6856a960e38f75911c75abc1ba3659f
Asad Ahmed <asad.ahmed.de@gmail.com>
2023-07-17 00:31:07 +0800

    Added jump scare trigger box

---
+++ Source/FrozenDread/Gameplay/JumpScareTriggerBox.cpp
@@ -0,0 +1,43 @@
+﻿//
+// JumpScare.cpp
+// Implementation of the AJumpScare class.
+//
+
+#include "JumpScareTriggerBox.h"
+
+#include "Components/AudioComponent.h"
+
+// Sets default values
+AJumpScareTriggerBox::AJumpScareTriggerBox()
+{
+	// Audio Component
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AudioComponent"));
+	AudioComponent->SetupAttachment(GetRootComponent());
+		
+	// Other settings
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AJumpScareTriggerBox::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	if (!IsTriggerEnabled)
+	{
+		return;
+	}
+	
+	if (TriggerSound)
+	{
+		AudioComponent->SetSound(TriggerSound);
+		AudioComponent->Play();
+	}
+
+	// Deactivate trigger if this is a one-shot jump-scare
+	if (IsOneShotTrigger)
+	{
+		IsTriggerEnabled = false;
+	}
+
+	// Notify delegate
+	OnJumpScareTriggered.Broadcast();
+}
+
---
+++ Source/FrozenDread/Gameplay/JumpScareTriggerBox.h
@@ -0,0 +1,50 @@
+﻿//
+// JumpScare.h
+// Definition of the AJumpScare class.
+//
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/TriggerBox.h"
+#include "JumpScareTriggerBox.generated.h"
+
+class USoundBase;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FJumpScareTriggerBoxDelegate);
+
+/**
+ * Trigger box that plays a jump-scare sound when the player enters an area
+ */
+UCLASS()
+class FROZENDREAD_API AJumpScareTriggerBox : public ATriggerBox
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AJumpScareTriggerBox();
+
+	UFUNCTION()
+	virtual void NotifyActorBeginOverlap(AActor* OtherActor) override;
+
+private:
+	/** Called when the jump scare is triggered */
+	UPROPERTY(BlueprintAssignable, Category="Jumpscare", meta=(AllowPrivateAccess="true"))
+	FJumpScareTriggerBoxDelegate OnJumpScareTriggered;
+	
+	/** Set to play the sound when player triggers the jump scare */
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Jumpscare", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<USoundBase> TriggerSound;
+
+	/** The audio component that will play the sounds */
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category="Jumpscare", meta=(AllowPrivateAccess="true"))
+	TObjectPtr<UAudioComponent> AudioComponent;
+
+	/** Set to true to only trigger once */
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category="Jumpscare", meta=(AllowPrivateAccess="true"))
+	bool IsOneShotTrigger { true };
+
+private:
+	bool IsTriggerEnabled { true };
+};
